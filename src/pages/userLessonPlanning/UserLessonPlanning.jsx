3198dc2d0fa974ab201494c82eeaaa99e9fc146b
Hans Bregman <hns101@live.nl>
2025-03-17 16:23:34 +0100

    starting the set up

--- src/pages/userLessonPlanning/UserLessonPlanning.jsx
+++ src/pages/userLessonPlanning/UserLessonPlanning.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
 import './UserLessonPlanning.css'
+import {useState} from "react";
 
 
 function UserLessonPlanning() {
+    const [data, setData] = useState();
+
     return (
         <>
+            <div className="user-lesson-planning">
+
+            </div>
 
         </>
     );
