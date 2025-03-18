8bc5f290ea8ab8f9aedc25e694ae7e9c2521572f
Hans Bregman <hns101@live.nl>
2025-03-18 19:16:50 +0100

    added styling & list fillers

--- src/pages/userLessonPlanning/UserLessonPlanning.jsx
+++ src/pages/userLessonPlanning/UserLessonPlanning.jsx
@@ -8,19 +8,27 @@ function UserLessonPlanning() {
     return (
         <>
             <main className="main">
-                <div className="user-lesson-planning">
+                <div className="lesson-outer-container">
                     <p className="test"> Les week {data.Week}</p>
-                    <div className=""></div>
-                    {data.Lessons.map((data) => (
-                        <div key={data.slot}>
-                            <p>{data.Date}</p>
-                            <p>{data.slot}</p>
-                            <p>{data.Time}</p>
-                            <div>{data.Students.map((data) => (<p key={data.id} >{data.firstname}</p>))}</div>
+                    <div className="lesson-container">
+                        {data.Lessons.map((data) => (
+                            <div key={data.slot} className="lesson">
+                                <div className="lesson-info">
 
+                                    <p className="lesson-info-slot">{data.slot}</p>
+                                    <p className="lesson-info-time">{data.Time}</p>
+                                    <p className="lesson-info-date">{data.Date}</p>
+                                </div>
+                                <div className="lesson-students">
+                                    {data.Students.map((data) =>
+                                        (<p key={data.id} className="lesson-student-name">{data.firstname}</p>))}
 
-                        </div>
-                        ))}
+                                    {[...Array(10 - data.Students.length)].map(() => (
+                                        <p key={data.id} className="lesson-student-filler"></p>))}
+                                </div>
+                            </div>
+                            ))}
+                    </div>
                 </div>
             </main>
         </>
